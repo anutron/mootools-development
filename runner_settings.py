c3da84efb35abe640bc9ffdc377f2f149f7a7056
Aaron Newton <git@clientcide.com>
2010-09-21 16:15:55 -0700

    Behavior environment

--- runner_settings.py
+++ runner_settings.py
@@ -51,11 +51,14 @@ DEPENDER_PACKAGE_YMLS = (
 #for example, ../core/package.yml for core
   os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "core", "package.yml")),
   os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "more", "package.yml")),
+  os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "behavior", "package.yml")),
+  os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "more-behaviors", "package.yml")),
   os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "art", "package.yml")),
   os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "touch", "package.yml")),
   os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "color", "package.yml")),
   os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "table", "package.yml")),
   os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "widgets", "package.yml")),
+  os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "slick", "package.yml")),
   os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "depender", "client", "package.yml")),
 )
 DEPENDER_SCRIPTS_JSON = []
@@ -69,7 +72,9 @@ MOOTOOLS_TEST_LOCATIONS = {
 #example: ../more/Tests
 
   "more": os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "more", "Tests")),
-  "widgets": os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "widgets", "Tests"))
+  "more-behaviors": os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "more-behaviors", "Tests")),
+  "widgets": os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "widgets", "Tests")),
+  "behavior": os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "behavior", "Tests")),
 }
 
 MAKO_TEMPLATE_DIRS = (
